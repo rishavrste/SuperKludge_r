52236b091a2b5b504625e1af398f24082fc9193e
Rishav Agrawal <rishavrste@gmail.com>
2026-02-23 12:12:33 +0800

    .

---
+++ PARIS_SAMPLING/imri_t_025_5_params.py
@@ -0,0 +1,374 @@
+import numpy as np
+import cupy as cp
+import matplotlib.pyplot as plt
+
+from itertools import product
+import os
+import h5py
+from tqdm import tqdm
+import pandas as pd
+
+#few utils
+from few.utils.utility import get_p_at_t
+from few.utils.constants import MTSUN_SI
+from few.utils.geodesic import get_fundamental_frequencies
+#few trajectory
+from few.trajectory.inspiral import EMRIInspiral
+from few.trajectory.ode.flux import SuperKludgeFlux
+#few waveform
+from few.waveform import FastKerrEccentricEquatorialFlux, GenerateEMRIWaveform
+from few.waveform.waveform import SuperKludgeWaveform
+from few.utils.constants import YRSID_SI
+
+#sef imports
+from stableemrifisher.fisher import StableEMRIFisher
+from stableemrifisher.utils import generate_PSD, padding, inner_product
+from stableemrifisher.fisher.derivatives import derivative
+from stableemrifisher.fisher.stablederivative import StableEMRIDerivative
+from stableemrifisher.noise import sensitivity_LWA
+
+#lisa-on-gpu import
+from fastlisaresponse import ResponseWrapper  # Response function 
+
+#LISAanalysistools imports
+from lisatools.detector import ESAOrbits, EqualArmlengthOrbits #ESAOrbits correspond to esa-trailing-orbits.h5, EqualArmlengthOrbits are equalarmlength-orbits.h5
+from lisatools.sensitivity import get_sensitivity, A1TDISens, E1TDISens, T1TDISens
+from lisatools.sensitivity import get_sensitivity,CornishLISASens
+
+use_gpu = True
+from parismc.sampler import SamplerConfig
+from parismc.sampler import Sampler
+
+from smt.sampling_methods import LHS
+
+if not use_gpu:
+    
+    import few
+    
+    #tune few configuration
+    cfg_set = few.get_config_setter(reset=True)
+    
+    cfg_set.enable_backends("cpu")
+    cfg_set.set_log_level("info");
+else:
+    pass #let the backend decide for itself
+
+#waveform class setup
+waveform_class = SuperKludgeWaveform
+max_step_days = 10.0 #max trajectory step size in days
+inspiral_kwargs = {
+    "err":1e-11, #default = 1e-11
+    "max_step_size":max_step_days*24*60*60, #in seconds
+    "use_gpu":use_gpu
+}
+sum_kwargs = {
+    "pad_output": True, # True if expecting waveforms smaller than LISA observation window.
+  #  "use_gpu":use_gpu
+}
+
+waveform_class_kwargs = dict(inspiral_kwargs=inspiral_kwargs,
+                              mode_selector_kwargs=dict(mode_selection_threshold=1e-5),
+                              sum_kwargs=sum_kwargs,
+                              use_gpu=use_gpu)
+
+waveform_generator = GenerateEMRIWaveform
+waveform_generator_kwargs = dict(return_list=False)
+
+if(use_gpu):
+    xp=cp
+else:
+    xp=np
+
+# [ 1.00000000e+06,  1.00000000e+04,  9.00000000e-01,
+#          2.85813146e+01,  5.00000000e-01,  1.00000000e+00,
+#          3.31765439e+01,  1.04719755e+00,  7.85398163e-01,
+#          6.28318531e-01,  5.23598776e-01,  1.00000000e-01,
+#          2.00000000e-01,  3.00000000e-01,  1.00000000e+01,
+#          2.50000000e-01,  9.50000000e-01]
+
+m1 = 1e6
+m2 = 1e4
+a = 0.9 
+p0 = 2.85813146e+01
+#p0 = 42.0
+e0 = 5.00000000e-01
+xI0 = 1.0
+dist = 3.31765439e+01/2
+#dist = 5.0
+qS = 1.04719755e+00
+phiS = 7.85398163e-01
+qK = 6.28318531e-01
+
+phiK =5.23598776e-01
+Phi_phi0 = 0.1
+Phi_theta0 =0.2
+Phi_r0 = 0.3
+
+dt = 10.0
+T = 0.25
+chi2 = 9.50000000e-01
+
+dev_0_p=0.0
+dev_0_e=0.0
+dev_1_p=0.0
+dev_1_e=0.0
+dev_2_p=0.0
+dev_2_e=0.0
+evolve_1PA = False
+evolve_primary = False
+evolve_2PA = False
+deviation_included=True
+
+
+print(use_gpu)
+pars_list_com = [m1, m2, a, p0, e0, xI0, dist, qS, phiS, qK, phiK, Phi_phi0, Phi_theta0, Phi_r0,\
+             chi2,evolve_1PA,evolve_primary,evolve_2PA,deviation_included,dev_0_p,dev_0_e,dev_1_p,dev_1_e,dev_2_p,dev_2_e]
+
+add_param_args={"chi2":chi2,"evolve_1PA":evolve_1PA,"evolve_primary":evolve_primary,"evolve_2PA":evolve_2PA,"deviation_included":deviation_included,"dev0p":dev_0_p,
+"dev0e":dev_0_e,"dev1p":dev_1_p,"dev1e":dev_1_e,"dev2p":dev_2_p,"dev2e":dev_2_e}
+
+param_names = ['m1','m2','a','p0','e0','qS','phiS','Phi_phi0','Phi_r0','dev0p','dev0e']
+add_args = [chi2, evolve_1PA, evolve_primary, evolve_2PA,deviation_included,dev_0_p,dev_0_e,dev_1_p,dev_1_e,dev_2_p,dev_2_e]
+
+emri_kwargs = {"T":T, "dt":dt}
+
+param_names_com = ['m1','m2','a','p0','e0','xI0','dist','qS','phiS','qK','phiK','Phi_phi0','Phi_theta0','Phi_r0',"chi2",
+               "evolve_1PA","evolve_primary","evolve_2PA","deviation_included","dev0p","dev0e","dev1p","dev1e","dev2p","dev2e"]
+
+der_order = 8
+Ndelta=10
+sef = StableEMRIFisher(waveform_class=waveform_class, 
+                       waveform_class_kwargs=waveform_class_kwargs,
+                       waveform_generator=waveform_generator,
+                       waveform_generator_kwargs=waveform_generator_kwargs,
+                       stats_for_nerds = True, use_gpu = use_gpu,
+                       deriv_type='stable',
+                       noise_model=get_sensitivity,
+                       noise_kwargs={'sens_fn':CornishLISASens,'return_type':'PSD'},
+                       channels=["A","E"],
+                       T = T, dt = dt,
+                       stability_plot = False,
+                       der_order = der_order, Ndelta = Ndelta,
+                       plunge_check=True, return_derivatives=False
+                       )
+                   
+
+SNR = sef.SNRcalc_SEF(*pars_list_com,**emri_kwargs,use_gpu=use_gpu)
+print("SNR: ", SNR)
+#initialize the 0PA (approximate) model
+evolve_1PA = False 
+evolve_primary = False
+evolve_2PA = False #for approximate model
+deviation_included=True
+add_param_args={"chi2":chi2,"evolve_1PA":evolve_1PA,"evolve_primary":evolve_primary,"evolve_2PA":evolve_2PA,"deviation_included":deviation_included,"dev0p":dev_0_p,
+"dev0e":dev_0_e,"dev1p":dev_1_p,"dev1e":dev_1_e,"dev2p":dev_2_p,"dev2e":dev_2_e}
+
+
+param_names = ['m1','m2','a','p0','e0']
+pars_list = [m1, m2, a, p0, e0, xI0, dist, qS, phiS, qK, phiK, Phi_phi0, Phi_theta0, Phi_r0]
+
+param_dict = {
+    'm1': m1,
+    'm2': m2,
+    'a': a,
+    'p0': p0,
+    'e0': e0,
+    'xI0': xI0,
+    'dist': dist,
+    'qS': qS,
+    'phiS': phiS,
+    'qK': qK,
+    'phiK': phiK,
+    'Phi_phi0': Phi_phi0,
+    'Phi_theta0': Phi_theta0,
+    'Phi_r0': Phi_r0
+}
+
+
+Fisher = sef(wave_params = param_dict,param_names=param_names, add_param_args=add_param_args,
+            live_dangerously = False, stability_plot = True,der_order = der_order, Ndelta = Ndelta,
+            )
+
+def logmasstransform(Fisher, m1, index_of_m1 = 0):    
+    J = np.eye(len(Fisher))
+    J[index_of_m1,index_of_m1] = m1
+    
+    return J.T@Fisher@J
+fisher_=logmasstransform(Fisher, m1, index_of_m1 = 0)
+cov=np.linalg.inv(fisher_)
+std= np.sqrt(np.diag(cov))
+params_truth_in = np.array([np.log(m1), m2, a, p0, e0])
+
+deviation_included=True
+evolve_1PA=True
+evolve_primary=False
+evolve_2PA=False
+use_gpu=True
+add_args = [chi2, evolve_1PA, evolve_primary, evolve_2PA,deviation_included,dev_0_p,dev_0_e,dev_1_p,dev_1_e,dev_2_p,dev_2_e]
+superkludge_wave = GenerateEMRIWaveform(SuperKludgeWaveform,\
+                                    sum_kwargs=sum_kwargs,\
+                                    return_list=True,
+                                    mode_selector_kwargs=dict(mode_selection_threshold=1e-5),
+                                    inspiral_kwargs=inspiral_kwargs,
+                                    use_gpu=use_gpu)
+print(add_args)
+
+waveform_true = superkludge_wave(m1, m2, a, p0, e0, xI0, dist, qS, phiS, qK, phiK, Phi_phi0, Phi_theta0, Phi_r0, *add_args, dt=dt, T=T)
+PSD=generate_PSD(waveform_true,dt,use_gpu=use_gpu,
+                noise_PSD=get_sensitivity,
+                noise_kwargs={'sens_fn':CornishLISASens,'return_type':'PSD'},
+                channels=["A","E"])
+waveform_true=xp.array(waveform_true)
+
+deviation_included=False
+evolve_1PA=False
+evolve_primary=False
+evolve_2PA=False
+add_args = [chi2, evolve_1PA, evolve_primary, evolve_2PA,deviation_included,dev_0_p,dev_0_e,dev_1_p,dev_1_e,dev_2_p,dev_2_e]
+
+def loglike_calc(m1_, m2_, a_, p0_, e0_):
+    waveform_temp=xp.array(superkludge_wave(m1_, m2_, a_, p0_, e0_, xI0, dist, qS, phiS, qK, phiK, Phi_phi0, Phi_theta0, Phi_r0, *add_args, dt=dt, T=T,use_gpu=use_gpu))
+    diff_inner=inner_product(waveform_true-waveform_temp,waveform_true-waveform_temp,PSD,dt,use_gpu=use_gpu)
+    return -0.5 * diff_inner
+
+
+def log_density(params):
+    params = np.asarray(params)
+    n_samples = params.shape[0] 
+    log_likes = np.zeros(n_samples)
+    for i in range(n_samples):
+        logm1_, m2_, a_, p0_, e0_ = params[i]
+        m1_ = np.exp(logm1_)
+        loglike = loglike_calc(m1_, m2_, a_, p0_, e0_)
+        log_likes[i] = loglike 
+    return log_likes
+
+n=30
+logm1lim = [max(0,params_truth_in[0] - n*std[0]), params_truth_in[0] + n*std[0]]
+m2lim = [max(0,params_truth_in[1] - n*std[1]), params_truth_in[1] + n*std[1]]
+alim = [max(-0.999,params_truth_in[2] - n*std[2]), min(params_truth_in[2] + n*std[2], 0.999)]  # a must be <1
+p0lim = [max(0,params_truth_in[3] - n*std[3]), params_truth_in[3] + n*std[3]]
+e0lim = [max(0,params_truth_in[4] - n*std[4]), min(1,params_truth_in[4] + n*std[4])]
+
+
+
+def prior_transform(u):
+
+    transformed = np.zeros_like(u)
+    # Uniform in log for masses
+    # m1
+    transformed[:, 0] = (logm1lim[1] - logm1lim[0]) * u[:, 0] + logm1lim[0]
+    transformed[:, 1] = (m2lim[1] - m2lim[0]) * u[:, 1] + m2lim[0]
+    transformed[:, 2] = (alim[1] - alim[0]) * u[:, 2] + alim[0]
+    transformed[:, 3] = (p0lim[1] - p0lim[0]) * u[:, 3] + p0lim[0] 
+    transformed[:, 4] = (e0lim[1] - e0lim[0]) * u[:, 4] + e0lim[0]
+
+
+    return transformed
+    
+def inverse_prior_transform(x):
+    
+    u = np.zeros_like(x)
+
+    u[:, 0] = (x[:, 0] - logm1lim[0]) / (logm1lim[1] - logm1lim[0])
+    u[:, 1] = (x[:, 1] - m2lim[0]) / (m2lim[1] - m2lim[0])
+    u[:, 2] = (x[:, 2] - alim[0]) / (alim[1] - alim[0])
+    u[:, 3] = (x[:, 3] - p0lim[0]) / (p0lim[1] - p0lim[0])
+    u[:, 4] = (x[:, 4] - e0lim[0]) / (e0lim[1] - e0lim[0])
+    
+    return u
+
+logm1lim = [max(0,params_truth_in[0] - n*std[0]), params_truth_in[0] + n*std[0]]
+m2lim = [max(0,params_truth_in[1] - n*std[1]), params_truth_in[1] + n*std[1]]
+alim = [max(-0.999,params_truth_in[2] - n*std[2]), min(params_truth_in[2] + n*std[2], 0.999)]  # a must be <1
+p0lim = [max(0,params_truth_in[3] - n*std[3]), params_truth_in[3] + n*std[3]]
+e0lim = [max(0,params_truth_in[4] - n*std[4]), min(1,params_truth_in[4] + n*std[4])]
+
+ranges=[logm1lim, m2lim, alim, p0lim, e0lim]
+
+# def main():
+    
+config = SamplerConfig(
+    merge_confidence=0.9,          # Coverage prob → Mahalanobis merge radius R_m (higher is more permissive)
+    alpha=10000,                    # Use recent samples for weighting
+    trail_size=int(1e3),          # Maximum trials per iteration
+    boundary_limiting=True,        # Enable boundary constraints
+    use_beta=True,                # Use beta correction for boundaries
+    integral_num=int(1e5),        # MC samples for beta estimation
+    gamma=500,                    # Covariance update frequency
+    exclude_scale_z=10,       # No exclusion based on weights
+    use_pool=False,               # Set to True for multiprocessing
+    cov_jitter=1e-10,             # Jitter added to covariance for stability
+
+    # n_pool=4                     # Number of processes (if use_pool=True)
+)
+
+ndim = 5
+n_seed = int(1e4)  # Number of initial processes
+init_cov_list = [np.eye(ndim) * 1e-10] * n_seed
+savepath = 'paris_1PA_vs_0PA_5_params'  # Directory to save results
+
+# Create save directory
+os.makedirs(savepath, exist_ok=True)
+
+print(f"Problem dimension: {ndim}")
+print(f"Number of processes: {n_seed}")
+print(f"Save path: {savepath}")
+print(f"Multiprocessing: {config.use_pool}")
+# Initialize sampler
+print("\nInitializing sampler...")
+sampler = Sampler(
+    ndim=ndim, 
+    n_seed=n_seed,
+    log_density_func=log_density,
+    init_cov_list=init_cov_list,
+    prior_transform=prior_transform,
+    config=config
+)
+   # Prepare initial samples using Latin Hypercube Sampling
+print("Preparing LHS samples...")
+
+#best value got so far
+true_point = np.array([0.5, 0.5, 0.5, 0.5, 0.5])
+    # Generate 99 points around it with tiny Gaussian noise ~1e-10
+scatter = 1.0e-7
+rng = np.random.default_rng(42)
+scatter = 1.0e-6
+points = true_point + rng.normal(size=(n_seed-1, 5)) * scatter
+external_lhs_points = np.vstack([points, true_point])
+print("Shape of points array:", external_lhs_points.shape)
+external_lhs_log_densities = log_density(prior_transform(external_lhs_points))
+print("true points in corrrect space",params_truth_in)
+print("external_lhs_log_densities", external_lhs_log_densities)
+
+
+sampler.run_sampling(
+            num_iterations=int(5e4),
+            savepath=savepath,
+            print_iter=100,
+            external_lhs_points=external_lhs_points,
+            external_lhs_log_densities=external_lhs_log_densities,
+            stop_dlogZ=0.01
+        )
+#except Exception as exc:
+ #       print(f"[WARN] PARIS sampling failed: {exc}")
+    
+# Gt results
+print("Extracting results...")
+samples_, weights_ = sampler.get_samples_with_weights(flatten=True)
+
+# Basic analysis
+print(f"\nResults Summary:")
+print(f"Total samples: {len(samples_)}")
+print(f"Effective sample size: {1/np.sum(weights_  **2):.1f}")
+
+# Weighted statistics
+weighted_mean = np.average(samples_, weights=weights_, axis=0)
+weighted_cov = np.cov(samples_.T, aweights=weights_)
+
+print(f"\nTrue values: {params_truth_in}")
+print(f"Estimated deviation: {weighted_mean}")
+print(f"Mean deviation: {np.linalg.norm(weighted_mean - params_truth_in[0:5]):.6f}")
+
+print(f"\nTrue covariance diagonal: {np.diag(cov)}")
+print(f"Estimated covariance diagonal: {np.diag(weighted_cov)}")
